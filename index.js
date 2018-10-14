5105d56659b9c68a6a2eafebe653155ccdf192e7
Unknown <craig.rmc@gmail.com>
2018-10-14 12:34:01 -0500

    Added dialog for full resume.

--- index.js
+++ index.js
@@ -19,3 +19,27 @@ function navMenuHide() {
   var navMenu = document.getElementById("navMenu");
   navMenu.className = navMenu.className.replace("w3-show", "");
 }
+
+// Send Resume Request
+function resumeDialogRequest() {
+  window.open('https://drive.google.com/file/d/12d3Xa7H2PYjiu-tDCI-k6sXdouoJNm5o/view?usp=sharing', '_blank');
+}
+
+// Show Resume Dialog
+function resumeDialogShow() {
+  var dialog = document.getElementById('enabledResumeDialog');
+  dialog.style.display = "block";
+}
+
+// Hide Resume Dialog
+function resumeDialogHide() {
+  var dialog = document.getElementById('enabledResumeDialog');
+  dialog.style.display = "none";
+}
+
+// Hide Resume Dialog
+window.onclick = function(event) {
+  if (event.target == document.getElementById('enabledResumeDialog')) {
+    resumeDialogHide();
+  }
+}
